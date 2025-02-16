cde544608dbe50bda7e15f9d8ad200257c69a1ce
Rafael Abreu <rafael0920abreu@gmail.com>
2025-02-16 19:57:35 -0300

    Atualizado os pop-ups dos projetos individuais

--- site/script.js
+++ site/script.js
@@ -1,12 +1,33 @@
 // Selecionando elementos
-const overlay = document.getElementById("overlay");
+const overlay_1 = document.getElementById("overlay_1");
+const overlay_2 = document.getElementById("overlay_2");
+const overlay_3 = document.getElementById("overlay_3");
+const overlay_4 = document.getElementById("overlay_4");
 
 // Função para abrir o pop-up
-function abrirPopup() {
-    overlay.classList.add("ativo");
+function abrirPopup_1() {
+    overlay_1.classList.add("ativo");
+}
+function abrirPopup_2() {
+    overlay_2.classList.add("ativo");
+}
+function abrirPopup_3() {
+    overlay_3.classList.add("ativo");
+}
+function abrirPopup_4() {
+    overlay_4.classList.add("ativo");
 }
 
 // Função para fechar o pop-up
-function fecharPopup() {
-    overlay.classList.remove("ativo");
+function fecharPopup_1() {
+    overlay_1.classList.remove("ativo");
+}
+function fecharPopup_2() {
+    overlay_2.classList.remove("ativo");
+}
+function fecharPopup_3() {
+    overlay_3.classList.remove("ativo");
+}
+function fecharPopup_4() {
+    overlay_4.classList.remove("ativo");
 }
